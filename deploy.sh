532eb59b2ff1897f0bca7aa56291aa6d090fbfa0
aggiechen <aggie1236@gmail.com>
2022-08-16 23:57:27 +0800

    upload profolio

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# sh deploy.sh => 輸入指令就會把master部署到gh_page
 
 # 發生錯誤時執行終止指令
 set -e
@@ -22,8 +23,8 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:aggie1236/aggie1236.github.io.git master:gh-pages
+# git push -f git@github.com:aggie1236/aggie1236.github.io.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-# git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
+git push -f https://github.com/aggie1236/aggie1236.github.io.git master:gh-pages
 
 cd -
